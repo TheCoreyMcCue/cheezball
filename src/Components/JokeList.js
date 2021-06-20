e0dc16e037d78c7b048dcc16126e68ca99db37a1
Corey McCue <coreysmccue@gmail.com>
2021-06-20 16:53:49 +0200

    initialize api get request

--- src/Components/JokeList.js
+++ src/Components/JokeList.js
@@ -1,6 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class JokeList extends Component {
+  static defaultProps = {
+    jokeStart: 10
+  };
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+  async componentDidMount() {
+    let joke = await axios.get('https://icanhazdadjoke.com/', {
+      headers: { Accept: "application/json" }
+    });
+  }
   render() {
     return (
       <div>

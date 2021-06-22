5bd837aa4b8a1a3a2df55f9f0658a1386fd044c7
Corey McCue <coreysmccue@gmail.com>
2021-06-22 19:06:22 +0200

    style Joke component (begin responsive design)

--- src/Components/Joke/Joke.js
+++ src/Components/Joke/Joke.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Joke.css'; 
 
 export default class Joke extends Component {
   render() {
@@ -6,7 +7,7 @@ export default class Joke extends Component {
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-          <span>{this.props.votes}</span>
+          <span className="Joke-votes">{this.props.votes}</span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
         </div>
         <div className="Joke-text">{this.props.text}</div>

1b7285218461ba6e7bd81090938d90b75834e39b
Corey McCue <coreysmccue@gmail.com>
2021-06-23 06:25:38 +0200

    more styles and add color responsiveness with voting

--- src/Components/Joke/Joke.js
+++ src/Components/Joke/Joke.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './Joke.css'; 
 
 export default class Joke extends Component {
+
+  getColor() {
+    if (this.props.votes >= 10) {
+      return "#006400";
+    } else if (this.props.votes >= 7) {
+      return "#38b000"
+    } else if (this.props.votes >= 5) {
+      return "#ccff33"
+    } else if (this.props.votes >= 3) {
+      return "#e4ff1a"
+    } else if (this.props.votes >= 2) {
+      return "#ffff3f"
+    } else if (this.props.votes >= 0) {
+      return "#ffa200"
+    } else {
+      return "#d90429"
+    }
+  };
+
   render() {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-          <span className="Joke-votes">{this.props.votes}</span>
+          <span className="Joke-votes" style={{borderColor: this.getColor()}}>{this.props.votes}</span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
         </div>
         <div className="Joke-text">{this.props.text}</div>
